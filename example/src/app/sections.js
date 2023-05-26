f0be07b11bccff90cb6675a7e23f46768b550594
AnjelicaB <hmni.anjelica@gmail.com>
2023-05-26 17:26:04 +0800

    What I want to include in my websites

---
+++ example/src/app/sections.js
@@ -0,0 +1,34 @@
+function AboutPage() {
+    <>
+      <h1>About</h1>
+      <p>
+        Hello! My name is Anjelica. 
+        <br />
+        I am a rising sophomore at Cornell University, and I plan on studying 
+        Electrical and Computer Engineering with a minor in Computer Science and
+        either maths or data science. 
+        <br />
+        I am in our school's robotics lab called the EmPRISE Robotics Lab, and 
+        I am in a professional business fraternity called Phi Chi Theta. That said, 
+        my professional interests lie at the intersection between technology and 
+        business. Beyond software engineering, I am also interested in consulting.
+        <br />
+        Outside of my academic and professional careers, I am an avid artist and 
+        I love trying out new things. I've explored the Cornell 
+        Rowing Club, Origami Club, some naturalist clubs and I'm looking to 
+        exploring more!
+      </p>
+    </>
+}
+
+function Artworks() {
+    // TODO
+}
+
+function Projects() {
+    // TODO
+}
+
+function Photos() {
+    // TODO
+}
