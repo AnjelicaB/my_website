4641e8891f15c9611bb4d7cb7bc9ebe80fa0b1da
AnjelicaB <hmni.anjelica@gmail.com>
2023-05-28 13:55:23 +0800

    displaying different section failed with buttons

--- example/src/app/page.js
+++ example/src/app/page.js
@@ -1,8 +1,33 @@
+'use client';
 import Image from 'next/image'
-import AboutPage from './sections.js'
-import MyButton from './sections.js'
+import {AboutPage} from './sections.js'
+import {Artworks} from './sections.js'
+import {Projects} from './sections.js'
+import {Photos} from './sections.js'
+import { useState } from 'react';
 
 export default function Home() {
+  const [state, setState] = useState(0);
+    
+  function handleClick({newState}) {
+    setState(newState);
+  }
+  
+  function displayedSection() {
+    switch (state) {
+        case 0:
+            return <AboutPage/>
+        case 1:
+            return <Artworks/>
+        case 2:
+            return <Projects/>
+        case 3:
+            return <Photos/>
+        default:
+            return <AboutPage/>
+    }
+  }
+
   return (
     <main>
     <div>
@@ -11,18 +36,22 @@ export default function Home() {
       </h1>
     </div>
 
-    <MyButton 
-      name= 'About'
-      state={0}
-        />
-    <MyButton 
-      name= 'Artworks'
-      state={1}
-    />
+    <MyButton name = "About" onClick={handleClick(0)} />
+    <MyButton name = "Artworks" onClick={handleClick(1)} />
+    <MyButton name = "Projects" onClick={handleClick(2)} />
+    <MyButton name = "Photos" onClick={handleClick(3)} />
 
     <div>
-      <AboutPage />
+      <displayedSection/>
     </div>
     </main>
   )
+}
+
+function MyButton({name, onClick}) {
+  return (
+      <button onClick={onClick}>
+          {name}
+      </button>
+  );
 }
--- example/src/app/sections.js
+++ example/src/app/sections.js
@@ -1,51 +1,68 @@
-let globalState = 0
+// function handleSwitch() {
+//     switch (globalState) {
+//         case 0:
+//             <AboutPage />
+//         case 1:
+//             Artworks();
+//         case 2:
+//             Projects();
+//         case 3:
+//             Photos();
+//     }
+// }
 
-function handleSwitch({globalState}) {
-    switch (globalState) {
-        case 0:
-            AboutPage();
-        case 1:
-            Artworks();
-        case 2:
-            Projects();
-        case 3:
-            Photos();
-    }
-}
+// export function Page() {
+//     const [state, setState] = useState(0);
+    
+//     function handleClick({newState}) {
+//         setState(newState);
+//     }
 
-function MyButton({name, state}) {
-    function handleClick() {
-        globalState = state
-    }
-    return (
-        <button
-            onClick={handleClick}>
-            {name}
-        </button>
+//     return (
+//         <>
+//         <MyButton name = "About" onClick={handleClick(0)} />
+//         <MyButton name = "Artworks" onClick={handleClick(1)} />
+//         <MyButton name = "Projects" onClick={handleClick(2)} />
+//         <MyButton name = "Photos" onClick={handleClick(3)} />
+
+//         {displayedSection(state)}
+//         </>
+//     )
+// }
+
+// export function MyButton({name, onClick}) {
+//     return (
+//         <button onClick={onClick}>
+//             {name}
+//         </button>
+//     );
+// }
+
+export function AboutPage() {
+    return(
+        <>
+        <p>
+        Hello! My name is Anjelica. 
+        <br />
+        <br />
+        I am a rising sophomore at Cornell University, and I plan on studying 
+        Electrical and Computer Engineering with a minor in Computer Science and
+        either maths or data science. 
+        <br />
+        <br />
+        I am in our school's robotics lab called the EmPRISE Robotics Lab, and 
+        I am in a professional business fraternity called Phi Chi Theta. That said, 
+        my professional interests lie at the intersection between technology and 
+        business. Beyond software engineering, I am also interested in consulting.
+        <br />
+        <br />
+        Outside of my academic and professional careers, I am an avid artist and 
+        I love trying out new things. I've explored the Cornell 
+        Rowing Club, Origami Club, some naturalist clubs and I'm looking to 
+        exploring more!
+        </p>
+        </>
     )
-}
-
-function AboutPage() {
-    <p>
-    Hello! My name is Anjelica. 
-    <br />
-    <br />
-    I am a rising sophomore at Cornell University, and I plan on studying 
-    Electrical and Computer Engineering with a minor in Computer Science and
-    either maths or data science. 
-    <br />
-    <br />
-    I am in our school's robotics lab called the EmPRISE Robotics Lab, and 
-    I am in a professional business fraternity called Phi Chi Theta. That said, 
-    my professional interests lie at the intersection between technology and 
-    business. Beyond software engineering, I am also interested in consulting.
-    <br />
-    <br />
-    Outside of my academic and professional careers, I am an avid artist and 
-    I love trying out new things. I've explored the Cornell 
-    Rowing Club, Origami Club, some naturalist clubs and I'm looking to 
-    exploring more!
-    </p>
 }
 
 function Artworks() {
