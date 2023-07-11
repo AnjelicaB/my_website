802c30e1710bbf2bcde55c7385ed944f1e202ad1
AnjelicaB <hmni.anjelica@gmail.com>
2023-07-11 19:24:25 +0200

    fixed about page, started artworks page

--- example/src/app/about/page.js
+++ example/src/app/about/page.js
@@ -15,7 +15,8 @@ export default function About() {
             <NavBar2/>
             <Intro />
             <Countries />
-            <Draft />
+            <Description />
+            <Footer />
         </div>
     )
 }
@@ -57,39 +58,62 @@ function Countries() {
     )
 }
 
-function Draft() {
+function Description() {
     return(
-    <div className='Draft'>
-        <section>
-            <p className='title'>ACADEMICS</p>
-            {/* <p className='Body'>
-            I am a rising sophomore at Cornell University, and I plan on studying 
-            Electrical and Computer Engineering with a minor in Computer Science and
-            either maths or data science.
-            </p> */}
-        </section>
-        <section>
-            <p className='title'>
-                CURRENTLY
-            </p>
-            {/* <p className='Body'>
-            I am in our school's robotics lab called the EmPRISE Robotics Lab, and 
-            I am in a professional business fraternity called Phi Chi Theta. That said, 
-            my professional interests lie at the intersection between technology and 
-            business. Beyond software engineering, I am also interested in consulting.
-            </p> */}
-        </section>
-        <section>
-            <p className='title'>
-                HOBBIES
-            </p>
-            {/* <p className='Body'>
+    <div className='description'>
+        <div className='mainBlockRow'>
+            <div className='block'>
+                <p className='title'>ACADEMICS</p>
+                <p className='body'>
+                    I am a sophomore at Cornell University, planning on studying 
+                    Electrical and Computer Engineering with a minor in Computer Science and
+                    either maths or data science.
+                </p> 
+            </div>
+            <div className='block'>
+                <p className='title'>CURRENTLY</p>
+                <p className='body'>
+                    I am in our school's EmPRISE Robotics Lab, the Phi Chi Theta 
+                    professional business fraternity and a member of the Development in 
+                    Games Association on the business subteam. That said, 
+                    my career interests lie at the intersection between technology and 
+                    business, namely engineering and consulting.
+                </p> 
+            </div>
+            <div className='block'>
+                <p className='title'>HOBBIES</p>
+                <p className='body'>
+                    I am an avid artist, mainly working with traditional media such as graphite and acrylic, but 
+                    I've been experimenting with digital art and animation. I've explored the Cornell 
+                    Rowing Club, Origami Club, some naturalist clubs and I'm looking to 
+                    exploring more! 
+                </p> 
+            </div>
+        </div>
+    </div>
+    )
+}
+
+function blah() {
+    <>
+    <p>
+    I am a rising sophomore at Cornell University, and I plan on studying 
+    Electrical and Computer Engineering with a minor in Computer Science and
+    either maths or data science.
+    </p> 
+
+    <p>
+    I am in our school's robotics lab called the EmPRISE Robotics Lab, and 
+    I am in a professional business fraternity called Phi Chi Theta. That said, 
+    my professional interests lie at the intersection between technology and 
+    business. Beyond software engineering, I am also interested in consulting.
+    </p> 
+
+    <p>
             Outside of my academic and professional careers, I am an avid artist and 
             I love trying out new things. I've explored the Cornell 
             Rowing Club, Origami Club, some naturalist clubs and I'm looking to 
             exploring more!
-            </p> */}
-        </section>
-    </div>
-    )
+            </p>
+    </>
 }
--- example/src/app/artworks/page.js
+++ example/src/app/artworks/page.js
@@ -3,29 +3,51 @@ import {exhibition} from "/public/pictures/IBArtworks/pictures.js";
 import Image from "next/image";
 import Link from 'next/link';
 
+import {NavBar2} from "../components/navBar/navBar.js"
+import styles from '../about/page.css'
+
 export default function Artworks() {
     return(
     <div>
-        <p>
-            Here is my art instagram account: @artisanjelica
-        </p>
-        
-        {/* IB artworks */}
-        <section>
-        <h2><Link href="/artworks/IB">IB Diploma exhibition</Link></h2>
-        <Image
-            src={exhibition.imageURL}
-            alt={exhibition.name}
-            width={exhibition.width/5}
-            height={exhibition.height/5}
-            // layout="responsive"
-        />
-        </section>
-
-        {/* Travel artworks */}
-        <section>
-        <h2><Link href="/artworks/Travel">Travel artworks</Link></h2>
-        </section>
+        <NavBar2 />
+        <Albums />
     </div>
     )
+}
+
+function Albums() {
+    return (
+    <div className="albums">
+        <div className="mainBlock">
+            <p className='title'>ACADEMICS</p>
+        </div>
+    </div>
+    )
+}
+
+function draft() {
+    return(
+        <div>
+            <p>
+                Here is my art instagram account: @artisanjelica
+            </p>
+            
+            {/* IB artworks */}
+            <section>
+            <h2><Link href="/artworks/IB">IB Diploma exhibition</Link></h2>
+            <Image
+                src={exhibition.imageURL}
+                alt={exhibition.name}
+                width={exhibition.width/5}
+                height={exhibition.height/5}
+                // layout="responsive"
+            />
+            </section>
+    
+            {/* Travel artworks */}
+            <section>
+            <h2><Link href="/artworks/Travel">Travel artworks</Link></h2>
+            </section>
+        </div>
+        )
 }
